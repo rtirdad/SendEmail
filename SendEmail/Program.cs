d49de8217f4d8a6e6af92e06fd6e44f5b2d76125
rtirdad <94053847+rtirdad@users.noreply.github.com>
2023-10-25 16:39:50 +0200

    changes in program

--- SendEmail/Program.cs
+++ SendEmail/Program.cs
@@ -1,16 +1,23 @@
 using Microsoft.Extensions.Configuration;
 using SendEmail.Services;
+using SendEmail.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
 
-
+builder.Services.AddTransient<IMailService, MailService>();
+builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IMailService, MailService>();
+
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin());
+});
+
 
 var app = builder.Build();
 
