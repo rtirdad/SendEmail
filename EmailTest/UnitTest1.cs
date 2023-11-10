f798b1767bfd4c63310d8958fa027475e8fa4f53
rtirdad <94053847+rtirdad@users.noreply.github.com>
2023-11-10 11:48:17 +0100

    program to class + WAF implemented

---
+++ EmailTest/UnitTest1.cs
@@ -0,0 +1,65 @@
+using FluentAssertions.Common;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using SendEmail.Models;
+using SendEmail.Services;
+using System.Net.Http.Json;
+
+namespace EmailTesting
+{
+    public class Tests
+    {
+        //private readonly FakeMailSender = new ();
+
+        [Test]
+        public async Task toEmailShould()
+        {
+            //Assign
+
+            var factory = new WebApplicationFactory<SendEmail.Program>().WithWebHostBuilder(builder => Setup(builder));
+
+
+            //Act
+            var httpClient = factory.CreateClient();
+
+            var mail = new MailRequest()
+
+            {
+
+                ToEmail = "ronat20003@gmail.com",
+
+                ToDisplayName = "rona",
+
+                FromDisplayName = "Rona",
+
+                FromMail = "ronat20003@gmail.com",
+
+                Subject = "Testing",
+
+                Body = "Hello",
+
+                //Attachments = []
+
+            };
+
+            //Assert
+            var respondse = await httpClient.PostAsJsonAsync<MailRequest>("/send", mail);
+            respondse.EnsureSuccessStatusCode();
+            
+        }
+        public void Setup(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+
+            {
+                services.AddSingleton<IMailService, FakeMailSender>();
+
+            });
+
+        }
+
+    }
+}
--- SendEmail/Services/IMailService.cs
+++ SendEmail/Services/IMailService.cs
@@ -8,4 +8,20 @@ namespace SendEmail.Services
         Task SendEmailAsync(MailRequest mailrequest);
 
     }
+
+    public class FakeMailSender : IMailService
+    {
+        public Task SendEmailAsync(MailRequest mailrequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MailKitSender : IMailService
+    {
+        public Task SendEmailAsync(MailRequest mailrequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
