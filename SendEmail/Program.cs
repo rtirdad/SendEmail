06e9dfa9a303906b5f1b2bb17c94f32da63e6d5f
rtirdad <94053847+rtirdad@users.noreply.github.com>
2024-01-29 11:02:29 +0100

    copied over code

--- SendEmail/Program.cs
+++ SendEmail/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.Extensions.Configuration;
 using SendEmail.Services;
 using SendEmail.Settings;
 using Serilog;
+using System.Text.RegularExpressions;
+using GemBox.Document;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -40,5 +43,59 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+app.MapPost("/report", (JsonDocument doc, HttpContext context) =>
+{
+    ComponentInfo.SetLicense("FREE-LIMITED-KEY");
+
+    var templateDirectory = @".\Templates";
+    var docRoot = doc.RootElement;
+
+    var docFormat = docRoot.GetProperty("format").ToString();
+    var templateName = docRoot.GetProperty("template").ToString();
+    var filePath = Path.Combine(templateDirectory, $"{templateName}.docx");
+    DocumentModel document = DocumentModel.Load(filePath);
+
+    var clientData = docRoot.GetProperty("data");
+
+    var pattern = @"\{{.*?\}}";
+    Regex rg = new Regex(pattern);
+    var matches = rg.Matches(document.Content.ToString());
+
+    foreach (Match match in matches)
+    {
+        var inWordSomewhere = match.Value;
+
+        var parts = inWordSomewhere.Split(new string[] { "{{", "}}", "." }, StringSplitOptions.RemoveEmptyEntries);
+
+        var current = clientData;
+        foreach (var part in parts)
+        {
+            current = current.GetProperty(part);
+        }
+        Console.WriteLine(current.ToString());
+        document.Content.Replace(inWordSomewhere, current.ToString());
+    }
+
+    document.Content.Replace(new Regex("{Date}", RegexOptions.IgnoreCase),
+        DateTime.Today.ToLongDateString());
+
+    var pdfSaveOptions = new PdfSaveOptions() { ImageDpi = 220 };
+
+    using var pdfStream = new MemoryStream();
+    var memoryStream = new MemoryStream();
+
+    pdfStream.CopyTo(memoryStream);
+    memoryStream.Position = 0;
+
+    document.Save(memoryStream, pdfSaveOptions);
+
+    return Results.File(memoryStream, "application/pdf", "report.pdf");
+})
+
+.WithName("Report")
+
+.WithOpenApi();
+
 
 app.Run();
