dd6a2aab701934dd04e6ad2bb7ca462c1ce1384f
rtirdad <94053847+rtirdad@users.noreply.github.com>
2024-01-31 13:35:03 +0100

    changes to sample json

--- SendEmail/Models/MailRequest.cs
+++ SendEmail/Models/MailRequest.cs
@@ -8,10 +8,15 @@ namespace SendEmail.Models
         public string ToEmail { get; set; }
         public string ToDisplayName { get; set; }
         public string FromDisplayName { get; set; }
+
         //public string FromMail { get; set; }
+
         public string Subject { get; set; }
+
         public string Body { get; set; }
-        //public IFormFile? Attachments { get; set; } 
+
+        //public IFormFile? Attachments { get; set; }
+        
         public string JsonData { get; set; }
 
         public JsonElement GetJsonDataAsJsonElement() => JsonDocument.Parse(JsonData).RootElement;
